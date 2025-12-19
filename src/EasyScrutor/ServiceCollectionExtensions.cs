b74bd39682eef26c58fbf6c9d318c6bcc4aa501b
Alex Dresko <me@alexdresko.com>
2025-12-18 23:38:39 -0500

    refactor: formatting

--- src/EasyScrutor/ServiceCollectionExtensions.cs
+++ src/EasyScrutor/ServiceCollectionExtensions.cs
@@ -47,7 +47,7 @@ public static class ServiceCollectionExtensions {
     /// <returns>The service collection for chaining.</returns>
     public static IServiceCollection AddEasyScrutorForAssembliesStartingWith(this IServiceCollection services, string prefix) {
         services.Scan(scan => scan
-        .FromDependencyContext(DependencyModel.DependencyContext.Default, assembly => 
+        .FromDependencyContext(DependencyModel.DependencyContext.Default, assembly =>
             assembly.FullName != null && assembly.FullName.StartsWith(prefix, StringComparison.OrdinalIgnoreCase))
         .AddClassesFromInterfaces());
 
@@ -63,7 +63,7 @@ public static class ServiceCollectionExtensions {
     /// <returns>The service collection for chaining.</returns>
     public static IServiceCollection AddEasyScrutorForAssembliesContaining(this IServiceCollection services, string text) {
         services.Scan(scan => scan
-        .FromDependencyContext(DependencyModel.DependencyContext.Default, assembly => 
+        .FromDependencyContext(DependencyModel.DependencyContext.Default, assembly =>
             assembly.FullName != null && assembly.FullName.Contains(text, StringComparison.OrdinalIgnoreCase))
         .AddClassesFromInterfaces());
 
